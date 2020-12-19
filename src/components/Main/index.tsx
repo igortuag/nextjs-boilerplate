299ca3fd7ae158823001ce87b0132339b6cbb08f
Igor Tuag <igortuag@gmail.com>
2020-12-19 11:09:07 -0300

    component structure

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,18 @@
+import * as S from './styles'
+
 const Main = () => (
   <main>
-    <h1>React Avançado</h1>
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado."
+      />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        TypeScript, ReactS, NextJS e Styled Components
+      </S.Description>
+      <S.Illustration src="/img/hero-illustration.svg" alt="Um desenvolvedor de frete para uma tela com código."/>
+    </S.Wrapper>
   </main>
 )
 
