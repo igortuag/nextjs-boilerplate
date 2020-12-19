a270efb9f119a7e959e35e462faf1d5b85d27d4f
Igor Tuag <igortuag@gmail.com>
2020-12-19 17:59:04 -0300

    add global styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,10 @@
+import GlobalStyles from '../src/styles/globals'
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  )
+]
