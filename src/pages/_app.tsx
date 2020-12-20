e03b9b01a0d30ff0b11042c70ac1dafb7bc6db07
Igor Tuag <igortuag@gmail.com>
2020-12-20 09:20:05 -0300

    fix main component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,19 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 
-import GlobalStyles from 'styles/globals'
+import GlobalStyles from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>React Avançado - Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png"/>
-        <link rel="apple-touch-icon" href="/img/icon-512.png"/>
-        <link rel='manifest' href='/manifest.json' />
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simples project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
         />
       </Head>
       <GlobalStyles />
